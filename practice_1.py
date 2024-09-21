883b7b09befacddfb9be819a06edd20ffa983b09
Kaniet <nakksenia@gmail.com>
2024-09-21 11:14:34 +0300

    very_first

---
+++ practice_1.py
@@ -0,0 +1,20 @@
+# Задание №1
+
+vid = input('Введите вид вашего питомца: ')
+age = input('Введите возраст вашего питомца: ')
+name = input('Введите кличку вашего питомца: ')
+
+print(f'Это {vid} по кличке "{name}". Возраст: {age}.')
+
+
+# Задание №2
+
+austral = input('Введите первый этап эволюции человека: ')
+homo_habilis = input('Введите второй этап эволюции человека: ')
+homo_erectus = input('Введите третий этап эволюции человеа:')
+homo_heidel= input('Введите четвертый этап эволюции человека:')
+neander = input('Введите пятый этап эволюции человека:')
+denisova = input('Введите шестой этап эволюции человека: ')
+sapiens = input('Введите седьмой этап эволюции человека: ')
+
+print(austral, homo_habilis, homo_erectus, homo_heidel, neander, denisova, sapiens, sep =" => ")
